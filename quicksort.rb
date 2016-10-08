02fc65c8709cc6a1e90ce1b162c1901bf738fb88
gorajski <nickgorajski@gmail.com>
2016-10-07 19:39:21 -0500

    Completes algorithm

--- quicksort.rb
+++ quicksort.rb
@@ -1,51 +1,64 @@
 def quicksort(array, start = 0, stop = array.length - 1)
-	# i = 0
-	# tally = 0
-
-	# p start
-	# p stop
-
-	# if array.length <= 1
-	# 	return array 
-	# end
-
-	# pivot = array.pop
-
-	# while i < array.length
-	# 	if array[i] < pivot
-	# 		array.unshift(array.delete_at(i))
-	# 		tally += 1
-	# 	end
-	# 	i += 1
-	# end
-
-	# array
-	# p 'start'
-	# p array.insert(tally, pivot)
-
-	# p front = quicksort(array[0, tally])
-	# p back = quicksort(array[tally + 1, array.length - 1])
-	# p '88888888888888888888888888888'
-	# p array = front + back
-
-	p array
 	i = start
-	wall = 0
-	pivot = stop
+	wall = start
+	pivot = array[stop]
 
+	# return [start, wall, stop] if stop - start < 1
+	return array if stop - start < 1
 
-	while i < array.length - 1
-		if pivot >= array[i]
+	while i < stop
+		if pivot > array[i]
 			array[wall], array[i] = array[i], array[wall]
 			wall += 1
 		end
-		i += 1
+		i += 1	
 	end
 
 	array[wall], array[stop] = array[stop], array[wall]
 
+	# [start, wall, stop]
 
-	p quicksort(array, start, wall-1) 
-	p quicksort(array, wall+1, stop)
+	quicksort(array, start, wall-1) 
+	quicksort(array, wall+1, stop)
 
 end
+
+# arr = [6,5,1,3,8,4,7,9,2]
+# arr = [7,3,7,6,8,1,9,5]
+# arr = [5,5,9,8,3,2,5,4,8]
+
+# puts "level 1"
+# output = quicksort(arr)
+# p arr
+# a = output[0]
+# b = output[1]
+# c = output[2]
+
+# 	puts "level 2a | #{a} - #{b-1}"
+# 	output = quicksort(arr, a, b-1)
+# 	p arr
+# 	x = output[0]
+# 	y = output[1]
+# 	z = output[2]
+
+# 		puts "level 2a i | #{x} - #{y-1}"
+# 		quicksort(arr, x, y-1)
+# 		p arr
+# 		puts "level 2a ii | #{y+1} - #{z}"
+# 		quicksort(arr, y+1, z)
+# 		p arr
+# 		# arr = [1, 2, 6, 3, 8, 4, 7, 9, 5]
+
+# 	puts "level 2b | #{b+1} - #{c}"
+# 	output = quicksort(arr, b+1, c)
+# 	p arr
+# 	x = output[0]
+# 	y = output[1]
+# 	z = output[2]
+
+# 		puts "level 2b i | #{x} - #{y-1}"
+# 		quicksort(arr, x, y-1)
+# 		p arr
+# 		puts "level 2b ii | #{y+1} - #{z}"
+# 		quicksort(arr, y+1, z)
+# 		p arr
--- spec/quicksort_spec.rb
+++ spec/quicksort_spec.rb
@@ -13,13 +13,13 @@ describe 'quicksort' do
 	end
 
 	it 'sorts the array' do
-		# expect(quicksort(@array1)).to eq [1,3,5,6,7,7,8,9]
-		# expect(quicksort(@array2)).to eq [2,3,4,5,5,5,8,8,9]
-		# expect(quicksort(@array3)).to eq [3,3,3,3,3,3,3,3]
+		expect(quicksort(@array1)).to eq [1,3,5,6,7,7,8,9]
+		expect(quicksort(@array2)).to eq [2,3,4,5,5,5,8,8,9]
+		expect(quicksort(@array3)).to eq [3,3,3,3,3,3,3,3]
 		expect(quicksort(@array7)).to eq [1,2,3,4,5,6,7,8,9]
 	end
 
-	pending 'sorts short array' do
+	it 'sorts short array' do
 		expect(quicksort(@array4)).to eq []
 		expect(quicksort(@array5)).to eq [6]
 		expect(quicksort(@array6)).to eq [0,7]
